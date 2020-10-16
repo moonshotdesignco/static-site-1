97b3efc95f18ec378bf5ce3ab21859cf75ef6a0c
harry <moonshotdesignco@gmail.com>
2020-10-15 20:31:46 -0400

    troubleshooting

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,17 @@
 let tsBtn = document.getElementById("theme-switch");
+let htmlEl = document.getElementById("html");
 let tsLblLi = document.getElementById("theme-switch-label-light");
 let tsLblDa = document.getElementById("theme-switch-label-dark");
 
 tsBtn.onclick = function() {
-    if (document.body.className == "light") {
+    if (htmlEl.className == "light") {
         document.body.className = "dark";
+        htmlEl.className = "dark";
         tsLblLi.classList.remove("active");
         tsLblDa.classList.add("active");
     } else {
         document.body.className = "light";
+        htmlEl.className = "light";
         tsLblLi.classList.add("active");
         tsLblDa.classList.remove("active");
     }
@@ -16,6 +19,7 @@ tsBtn.onclick = function() {
 
 function setTheme() {
     document.body.className = "light";
+    htmlEl.className = "light";
     tsLblLi.classList.add("active");
 }
 
